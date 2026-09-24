261fd840293a52b4904083478d5b6954c062f1dd
abduraufov_asilbek <abduraufovasilbek24@gmail.com>
2026-09-24 15:56:23 +0500

    l

--- src/components/headerni tagi/headernitag.jsx
+++ src/components/headerni tagi/headernitag.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./headernitag.css"
 import ikkinchi from '../images/ikkichirasm.jpg'
-
+import rasm3 from '../images/yumaloq.png'
+import rasm4 from '../images/uchburchak.png'
 const headernitag = () => {
   return (
-    <div className='w-[1440px] h-[1200px] bg-white'>
-      <div className='position: absolute justify-center left-[420px] top-[1100px] w-[1081px] h-[280px] bg-black'
+    <div className='h-[1200px] bg-white'>
+      <div className='justify-center w-[1081px] m-auto'
       >
-        <h1 className='font-gilroy text-[60px]' >Crafting Your Dream Home with Precision & Care</h1>
-        <p>Renovating your home should be seamless and inspiring. At ICal Construction, we simplify the remodeling journey with our expert design-build approach. From concept to completion, our skilled team ensures every detail is handled with precision and care. With decades of experience, we specialize in creating stunning kitchens, luxurious bathrooms, and elegant home transformations. Let’s bring your vision to life—stress-free and beautifully executed.</p>
+        <h1 className='font-gilroy text-center text-black text-[60px]  pt-[160px]' >Crafting Your Dream Home with Precision & Care</h1>
+        <p className='w-[1081px] text-black text-center text-[20px] mt-[16px]'>Renovating your home should be seamless and inspiring. At ICal Construction, we simplify the remodeling journey with our expert design-build approach. From concept to completion, our skilled team ensures every detail is handled with precision and care. With decades of experience, we specialize in creating stunning kitchens, luxurious bathrooms, and elegant home transformations. Let’s bring your vision to life—stress-free and beautifully executed.</p>
       </div>
-      <img className='w-[1440px] h-[617px] position: absolute top-[1467px] left-[320px] ' src={ikkinchi} alt="" />
 
+      <div className="rasmuchun ">
 
+        <img className='w-[1280px] m-auto mt-[87px] ' src={ikkinchi} alt="" />
+        <img src={rasm3} alt="" />
+        <img src={rasm4} alt="" />
+      </div>
 
 
 
